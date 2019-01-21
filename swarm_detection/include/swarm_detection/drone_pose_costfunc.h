1fb59c1949925a284c92fcce5b628bfc5d5420b3
Hao XU <xuhao3e8@gmail.com>
2019-01-21 22:49:40 +0800

    working on detection BA

--- swarm_detection/include/swarm_detection/drone_pose_costfunc.h
+++ swarm_detection/include/swarm_detection/drone_pose_costfunc.h
@@ -2,6 +2,7 @@
 #include <swarm_detection/swarm_detect_types.h>
 
 
+
 template <typename T>
 inline void InverseTrans(const T pose[7], T pose_output[7]) {
     pose_output[0] = - pose[0];
@@ -14,11 +15,14 @@ inline void InverseTrans(const T pose[7], T pose_output[7]) {
 }
 
 template <typename T>
-inline void AppleTrans2Point(const T pose[7],const T point[3], T p[3]) {
+inline void ApplyTrans2Point(const T pose[7],const T point[3], T p[3]) {
     QuaternionRotatePoint(pose, point, p);
     p[0] += pose[4];
     p[1] += pose[5];
     p[2] += pose[6];
+//    p[0] = pose[4] + point[0];
+//    p[1] = pose[5] + point[1];
+//    p[2] = pose[6] + point[2];
 }
 
 template <typename T>
@@ -59,46 +63,46 @@ struct DronePoseReprojectionError {
 
     }
 
-    
     template <typename T>
-    bool operator()(const T* const pose,
+    Eigen::Matrix<T, 3, 1> Point3dtoProj(const Camera * cam_def, const MarkerCornerObservsed & mco, const T * pose) const {
+        T Pmpni[3];
+        Vector3d pmpni = mco.rel_corner_pos();
+        Pmpni[0] = T(pmpni.x());
+        Pmpni[1] = T(pmpni.y());
+        Pmpni[2] = T(pmpni.z());
+
+        T point_to_proj[3];
+
+        ApplyTrans2Point(pose, Pmpni, point_to_proj);
+
+        //Point should on camera
+        return Eigen::Matrix<T, 3, 1>(point_to_proj[0], point_to_proj[1], point_to_proj[2]);
+    }
+
+    template <typename T>
+    Eigen::Matrix<T, 2, 1> PredictPoint(const Camera * cam_def, const MarkerCornerObservsed & mco, const T * pose) const {
+        Eigen::Matrix<T, 3, 1>  P = Point3dtoProj(cam_def, mco, pose);
+        Eigen::Matrix<T, 2, 1> predict_point;
+
+        cam_def->project_to_camera(P, predict_point);
+
+        return predict_point;
+    }
+
+    template <typename T>
+    bool operator()(T const* const* parameters,
                     T* residuals) const {
+        const T * pose = parameters[0];
         //Pose 0,1,2,3 quat
         //Pose 4,5,6 position
         int res_count = 0;
         for (int i = 0; i < point_by_cam.size(); i ++) {
             const Camera * cam_def = cam_array[i];
-            for (MarkerCornerObervsed mco : point_by_cam[i]) {
-                //Calculate a residual
-                // residuals[res_count] = 
-                //proj(Tcam^-1 * Tik * Pmpni) -Zn
-                //Tik is pose of target drone, i.e pose
-                T cam_pose[7];
-                T _tmp[7];
-                T _tmp2[7];
-
-                TransFromVecQuat(cam_def->pos(), cam_def->att(), cam_pose);
-
-                T Pmpni[3];
-                Vector3d pmpni = mco.rel_corner_pos();
-                Pmpni[0] = T(pmpni.x());
-                Pmpni[1] = T(pmpni.y());
-                Pmpni[2] = T(pmpni.z());
-
-                T point_to_proj[3];
-                
-                InverseTrans(cam_pose, _tmp);
-                MultiplyTrans(_tmp, pose, _tmp2);
-
-                AppleTrans2Point(_tmp2, Pmpni, point_to_proj);
-
-                //Point should on camera
-                T predict_point[2];
-                cam_def->project_to_camera(point_to_proj, predict_point);
-
-                residuals[res_count] = predict_point[0] - mco.observed_point.x();
+            for (auto mco : point_by_cam[i]) {
+                Eigen::Matrix<T, 2, 1> predict_point = PredictPoint(cam_def, mco, pose);
+                residuals[res_count] = predict_point(0) - mco.observed_point.x();
                 res_count ++;
-                residuals[res_count] = predict_point[1] - mco.observed_point.y();
+                residuals[res_count] = predict_point(1) - mco.observed_point.y();
                 res_count ++;
             }
         }
@@ -107,9 +111,10 @@ struct DronePoseReprojectionError {
 
     // Factory to hide the construction of the CostFunction object from
     // the client code.
-    static ceres::CostFunction* Create(std::vector<corner_array> _point_by_cam, camera_array _ca) {
-        return (new ceres::AutoDiffCostFunction<
-                DronePoseReprojectionError, ceres::DYNAMIC, 7>(
+    static ceres::DynamicAutoDiffCostFunction<DronePoseReprojectionError, 4>* 
+        Create(std::vector<corner_array> _point_by_cam, camera_array _ca) {
+        return (new ceres::DynamicAutoDiffCostFunction<
+                DronePoseReprojectionError, 4>(
                     new DronePoseReprojectionError( _point_by_cam, _ca)));
     }
 
--- swarm_detection/src/drone_pose_estimator.cpp
+++ swarm_detection/src/drone_pose_estimator.cpp
@@ -1,6 +1,10 @@
 #include <swarm_detection/drone_pose_estimator.h>
 #include <assert.h> 
 #include <swarm_detection/drone_pose_costfunc.h>
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+
+using namespace ros;
 
 inline float rand_FloatRange(float a, float b)
 {
@@ -8,48 +12,118 @@ inline float rand_FloatRange(float a, float b)
 }
 
 
-DronePoseEstimator::DronePoseEstimator(SwarmDroneDefs  & _drone_defs, camera_array & _ca):
-    drone_defs(_drone_defs), ca(_ca){
+DronePoseEstimator::DronePoseEstimator(SwarmDroneDefs  & _r_drone_defs, camera_array & _sca):
+    remote_drone_defs(_r_drone_defs), self_ca(_sca){
     
 }
 
-Pose DronePoseEstimator::estimate_drone_pose(std::vector<corner_array> & point_by_cam){
-    assert(point_by_cam.size() == ca.size());
+double DronePoseEstimator::estimate_drone_pose(std::vector<corner_array> & point_by_cam){
+    assert(point_by_cam.size() == self_ca.size());
     Pose pose;
     for (int i = 0; i < 10; i++) {
-        pose.pos.x() = rand_FloatRange(0.2, 5);
-        pose.pos.y() = rand_FloatRange(-2, 2);
-        pose.pos.z() = rand_FloatRange(3, 3);
+        pose.pos.x() = rand_FloatRange(-0.3, 0.3);
+        pose.pos.y() = rand_FloatRange(-0.3, 0.3);
+        pose.pos.z() = rand_FloatRange(0.1, 0.5);
 
         pose.quat = Quaterniond(AngleAxisd(
             rand_FloatRange(-3.14, 3.14),
             Vector3d(rand_FloatRange(-1, 1), rand_FloatRange(-1, 1), rand_FloatRange(-1, 1))
         ));
 
-        this->estimate_drone_pose(point_by_cam, pose);
+        double cost = this->estimate_drone_pose(point_by_cam, pose);
+        if (cost < 1.0) {
+            return cost;
+        }
     }
 
 }
 
+void DronePoseEstimator::draw(double x[], std::vector<corner_array> & point_by_cam) {
+    auto err = new DronePoseReprojectionError( point_by_cam, self_ca);
+    cv::Mat new_to_draw = mat_to_draw;
+    const Camera * cam_def = self_ca[0];
+    for (auto mco : point_by_cam[0]) {
+        std::cout << "mco pos" << mco.rel_corner_pos() << std::endl;
+        auto point3d = err->Point3dtoProj<double>(cam_def, mco, x);
+        auto predict_point = err->PredictPoint<double>(cam_def, mco, x);
+        cv::Point p;
+        p.x = predict_point(0)*2 + 640;
+        p.y = predict_point(1)*2 + 512;
+        printf("p3d %f %f %f ,pp %d %d\n",point3d(0), point3d(1), point3d(2), p.x, p.y);
+        cv::circle(new_to_draw, p, 10, cv::Scalar(0, 0,255), -1);
 
-Pose DronePoseEstimator::estimate_drone_pose(std::vector<corner_array> & point_by_cam, Pose initial_pose) {
+        p.x = mco.observed_point.x()*2 + 640;
+        p.y = mco.observed_point.y()*2 + 512;
+        printf("op %d %d\n", p.x, p.y);
+
+        cv::circle(new_to_draw, p, 10, cv::Scalar(255,0,0), -1);
+
+    }
+
+    cv::resize(new_to_draw, new_to_draw, cv::Size(640, 512));
+
+    cv::imshow("predict", new_to_draw);
+    cv::waitKey(-1);
+
+}
+
+double DronePoseEstimator::estimate_drone_pose(std::vector<corner_array> & point_by_cam, Pose initial_pose) {
     Problem ba_problem;
-    ceres::CostFunction* cost_func = DronePoseReprojectionError::Create(point_by_cam, ca);
+    ceres::DynamicAutoDiffCostFunction<DronePoseReprojectionError, 4>* 
+        cost_func = DronePoseReprojectionError::Create(point_by_cam, self_ca);
+    int num_res = 0;
+    for (auto ca : point_by_cam) {
+        num_res += ca.size()*2;
+    }
+
+    printf("Number of residual is %d", num_res);
+    cost_func ->SetNumResiduals(num_res);
+    cost_func->AddParameterBlock(7);
+
     double x[7];
     initial_pose.to_vector(x);
+    printf("Init Quat %3.2f %3.2f %3.2f %3.2f pos %3.2f %3.2f %3.2f\n\n",
+        x[0],x[1],x[2],x[3],x[4],x[5],x[6]);
+
+
+
     ba_problem.AddResidualBlock(cost_func,NULL, x);
-    
+    for (int i = 0; i < 4 ; i ++)
+    {
+        ba_problem.SetParameterLowerBound(x, i, -0.1);
+        ba_problem.SetParameterUpperBound(x, i, 0.1);
+    }
+
+    for (int i = 4; i < 7 ; i ++)
+    {
+        ba_problem.SetParameterLowerBound(x, i, -2);
+        ba_problem.SetParameterUpperBound(x, i, 2);
+    }
+
+    ba_problem.SetParameterLowerBound(x, 6, 0);
+    ba_problem.SetParameterUpperBound(x, 6, 2);
+
+    Pose pose;
     ceres::Solver::Options options;
     options.linear_solver_type = ceres::DENSE_SCHUR;
-    options.minimizer_progress_to_stdout = true;
+    options.minimizer_progress_to_stdout = false;
     
     ceres::Solver::Summary summary;
     ceres::Solve(options, &ba_problem, &summary);
-    std::cout << summary.FullReport() << "\n";
+    std::cout << summary.BriefReport() << "\n";
 
+    printf("Quat %3.2f %3.2f %3.2f %3.2f pos %3.2f %3.2f %3.2f\n\n",
+        x[0],x[1],x[2],x[3],x[4],x[5],x[6]);
+
+    if (enable_drawing) {
+        draw(x, point_by_cam);
+    }
+    return summary.final_cost;
 }
 
 
 // SwarmPosesEstimator::SwarmPosesEstimator(std::string swarm_maker_config) {
 
 // }
+
+
