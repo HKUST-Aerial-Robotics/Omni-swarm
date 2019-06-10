9f0689299497f6b1a50bef2d031130783cfcdd4b
XU Hao <xuhao3e8@gmail.com>
2019-06-10 15:06:07 +0800

    Delete useless optimization

--- swarm_localization/include/swarm_localization/localiztion_costfunction.hpp
+++ swarm_localization/include/swarm_localization/localiztion_costfunction.hpp
@@ -88,22 +88,9 @@ struct SwarmFrameError {
     int self_id = -1;
     bool is_lastest_frame = false;
     Pose self_pose;
-    std::vector<NodeFrame> nfs;
-    std::vector<int> ids;
-    int64_t ts;
-    int nf_num = 0;
-    std::vector<Eigen::Vector3d> ann_poss;
-    std::vector<std::vector<int>> id_detected_indexs;
-    std::vector<std::vector<Eigen::Vector3d>> id_detected_poscov;
-    std::vector<std::vector<Eigen::Vector3d>> id_detected_angcov;
-    std::vector<std::vector<int>> id_distance_indexs;
-    std::vector<std::vector<double>> id_dis;
 
-    inline int safe_index(int index) const {
-        return (index + nf_num) % nf_num;
-    }
     template<typename T>
-    inline void get_pose_by_id(int _id, T const *const *_poses, T * t_pose) const {
+    inline void get_pose(int _id, T const *const *_poses, T * t_pose) const {
 //        printf("%d", _id);
         if (_id == self_id && is_lastest_frame) {
             self_pose.to_vector_xyzyaw(t_pose);
@@ -115,44 +102,29 @@ struct SwarmFrameError {
             t_pose[2] =  _poses[index][2];
             t_pose[3] =  _poses[index][3];
         }
+
     }
 
     template<typename T>
-    inline void get_pose_by_index(int index, T const *const *_poses, T * t_pose) const {
-//        printf("%d", _id);
-        if (index == -1) {
-            self_pose.to_vector_xyzyaw(t_pose);
-            return;
-        } else {
-            t_pose[0] =  _poses[index][0];
-            t_pose[1] =  _poses[index][1];
-            t_pose[2] =  _poses[index][2];
-            t_pose[3] =  _poses[index][3];
-        }
-    }
-
-    template<typename T>
-    inline void estimate_relpose(int indexa, int indexb, T const *const *_poses, T *relpose) const {
-        T posea[4] , poseb[4];
-        get_pose_by_index(indexa, _poses, posea);
-        get_pose_by_index(indexb, _poses, poseb);
+    inline void estimate_relpose(int ida, int idb, T const *const *_poses, T *relpose) const {
+        T posea[7] , poseb[7];
+        get_pose(ida, _poses, posea);
+        get_pose(idb, _poses, poseb);
         DeltaPose(posea, poseb, relpose);
     }
 
     //Need add anntena position here!
     template<typename T>
-    inline T node_distance(int indexi, int indexj, T const *const *_poses) const {
+    inline T node_distance(int idi, int idj, T const *const *_poses) const {
         //If consider bias here?
-        T posea[4] , poseb[4];
-        get_pose_by_index(indexi, _poses, posea);
-        get_pose_by_index(indexj, _poses, poseb);
+        T posea[7] , poseb[7];
+        get_pose(idi, _poses, posea);
+        get_pose(idj, _poses, poseb);
 
         T pa[3], pb[3];
         T p_anna[3], p_annb[3];
-        
-
-        EigenVec2T( ann_poss[safe_index(indexi)], p_anna);
-        EigenVec2T( ann_poss[safe_index(indexj)], p_annb);
+        EigenVec2T(sf.id2nodeframe.at(idi).get_anntena_pos(), p_anna);
+        EigenVec2T(sf.id2nodeframe.at(idj).get_anntena_pos(), p_annb);
         PoseTransformPoint(posea, p_anna, pa);
         PoseTransformPoint(poseb, p_annb, pb);
 
@@ -163,33 +135,31 @@ struct SwarmFrameError {
 
 
     template<typename T>
-    int nodeframe_distance_res(int index, T const *const *_poses, T *_residual, int res_count) const {
-        for (int i = 0; i < id_distance_indexs[safe_index(index)].size(); i ++) {
-            int _indexj = id_distance_indexs[safe_index(index)][i];
-            T _dis = T(id_dis[safe_index(index)][i]);
+    inline int nodeframe_distance_res(NodeFrame &_nf, T const *const *_poses, T *_residual, int res_count) const {
+        for (auto it : _nf.dis_map) {
+            int _idj = it.first;
+            T _dis = T(it.second);
             //Less accuracy on distance
-            _residual[res_count] = (node_distance(index, _indexj, _poses) - _dis) *ERROR_NORMLIZED/ DISTANCE_MEASURE_ERROR;
+            _residual[res_count] = (node_distance(_nf.id, _idj, _poses) - _dis) *ERROR_NORMLIZED/ DISTANCE_MEASURE_ERROR;
             res_count++;
         }
         return res_count;
     }
 
     template<typename T>
-    inline int nodeframe_relpose_res(int index, T const *const *_poses, T *_residual, int res_count) const {
-        const NodeFrame & _nf = nfs[safe_index(index)];
+    inline int nodeframe_relpose_res(NodeFrame &_nf, T const *const *_poses, T *_residual, int res_count) const {
         for (auto it: _nf.detected_nodes) {
-            //Detected pose error
-            int _idj = it.first;
-            int _indexj = id2poseindex.at(_idj);
+//            Detected pose error
+            int _id = it.first;
             Pose _rel_pose = it.second;
             T rel_pose[4];
             _rel_pose.to_vector_xyzyaw(rel_pose);
 
             T relpose_est[4];
-            estimate_relpose(index, _indexj, _poses, relpose_est);
+            estimate_relpose(_nf.id, _id, _poses, relpose_est);
 
-            Eigen::Vector3d pos_cov = _nf.detected_nodes_poscov.at(_idj);
-            Eigen::Vector3d ang_cov = _nf.detected_nodes_angcov.at(_idj);
+            Eigen::Vector3d pos_cov = _nf.detected_nodes_poscov[_id];
+            Eigen::Vector3d ang_cov = _nf.detected_nodes_angcov[_id];
 
             pose_error(relpose_est, rel_pose, _residual + res_count, pos_cov, ang_cov.z());
             res_count = res_count + 4;
@@ -228,21 +198,19 @@ struct SwarmFrameError {
     bool operator()(T const *const *_poses, T *_residual) const {
         int res_count = 0;
 
-        for (int i =0; i < nfs.size(); i ++) {
+        for (auto it : sf.id2nodeframe) {
+//            auto _id = it.first;
+            NodeFrame &_nf = it.second;
+
             //First we come to distance error
-            const NodeFrame _nf = nfs[i];
-            int index = i;
-            if (is_lastest_frame && i == nfs.size() - 1) {
-                index = -1;
-            }
             if (_nf.frame_available) {
-                
+
                 if (_nf.dists_available) {
-                    res_count = nodeframe_distance_res(index, _poses, _residual, res_count);
+                    res_count = nodeframe_distance_res(_nf, _poses, _residual, res_count);
                 }
 
                 if (_nf.has_detect_relpose) {
-                    res_count = nodeframe_relpose_res(index, _poses, _residual, res_count);
+                    res_count = nodeframe_relpose_res(_nf, _poses, _residual, res_count);
                 }
 
             }
@@ -254,44 +222,11 @@ struct SwarmFrameError {
     }
 
     SwarmFrameError(const SwarmFrame &_sf, const std::map<int, int> &_id2poseindex, bool _is_lastest_frame) :
-            sf(_sf),
-            id2poseindex(_id2poseindex),
+            sf(std::move(_sf)),
+            id2poseindex(std::move(_id2poseindex)),
             is_lastest_frame(_is_lastest_frame) {
-                //Must ensure id2poseindex not contain self id in last pos
-        nf_num = sf.id2nodeframe.size();
-
-        self_id = _sf.self_id;
-        self_pose = sf.id2nodeframe.at(self_id).pose();
-
-        nfs = std::vector<NodeFrame>(nf_num);
-        ids = std::vector<int>(nf_num);
-        ann_poss = std::vector<Eigen::Vector3d>(nf_num);       
-        id_distance_indexs = std::vector<std::vector<int>>(nf_num);
-        id_dis = std::vector<std::vector<double>>(nf_num);
-        for (auto it : sf.id2nodeframe) {
-            int _id = it.first;
-            NodeFrame &_nf = it.second;
-            if (_id == self_id && is_lastest_frame) {
-                nfs[nf_num - 1] = _nf;
-                ids[nf_num - 1] = _id;
-                ann_poss[nf_num - 1] = _nf.get_anntena_pos();
-                id2poseindex[_id] = -1;
-
-            } else {
-                int _index = id2poseindex[_id];
-                nfs[_index] = _nf;
-                ids[_index] = _id; 
-                ann_poss[_index] = _nf.get_anntena_pos();
-            }
-
-            int _index = id2poseindex[_id];
-            for (auto it_dismap : _nf.dis_map) {
-                id_distance_indexs[safe_index(_index)].push_back(id2poseindex[it_dismap.first]);
-                id_dis[safe_index(_index)].push_back(it_dismap.second);
-            }
-        }
-        ts = sf.ts;
-
+            self_id = _sf.self_id;
+            self_pose = sf.id2nodeframe.at(self_id).pose();
     }
 };
 
@@ -303,14 +238,13 @@ struct SwarmHorizonError {
     int64_t last_ts = -1;
 
     std::vector<Pose> delta_poses;
-    Pose _last_pose;
 
     bool is_self_node = false;
     SwarmHorizonError(const std::vector<NodeFrame> &_nf_win, const std::map<int64_t, int> &_ts2poseindex, bool _is_self_node) :
             nf_windows(_nf_win),
             ts2poseindex(_ts2poseindex),
             is_self_node(_is_self_node){
-        //Must ensure poses is sort by ts
+
         for (unsigned int i = 1; i< _nf_win.size(); i++) {
             auto _nf = _nf_win[i];
 //            std::cout << i << ":" << _nf_win[i-1].pose().position << std::endl;
@@ -321,15 +255,17 @@ struct SwarmHorizonError {
 //            printf("\n");
         }
         last_ts = nf_windows.back().ts;
-        _last_pose = nf_windows.back().pose();
     }
 
     template<typename T>
-    inline void get_pose(int index, T const *const *_poses, T * t_pose) const {
-        if (is_self_node && index == nf_windows.size() - 1) {
-            _last_pose.to_vector_xyzyaw(t_pose);
+    inline void get_pose(int64_t ts, T const *const *_poses, T * t_pose) const {
+
+        if (is_self_node && ts == last_ts) {
+            Pose _pose = nf_windows.back().pose();
+            _pose.to_vector_xyzyaw(t_pose);
             return;
         } else {
+            int index = ts2poseindex.at(ts);
             t_pose[0] =  _poses[index][0];
             t_pose[1] =  _poses[index][1];
             t_pose[2] =  _poses[index][2];
@@ -356,8 +292,8 @@ struct SwarmHorizonError {
             int64_t tsb = nf_windows[i+1].ts;
 
             //
-            get_pose(i, _poses, est_posea);
-            get_pose(i + 1, _poses, est_poseb);
+            get_pose(tsa, _poses, est_posea);
+            get_pose(tsb, _poses, est_poseb);
 
 
            Eigen::Vector3d pos_cov = Eigen::Vector3d(1, 1, 1) * VO_DRIFT_METER;
