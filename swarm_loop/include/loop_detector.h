bfa138aba28172e3f8d5f09458a140aaf7c64444
XU Hao <xuhao3e8@gmail.com>
2019-11-21 15:06:58 +0800

    Modified

--- swarm_loop/include/loop_detector.h
+++ swarm_loop/include/loop_detector.h
@@ -5,12 +5,13 @@
 #include <Eigen/Eigen>
 #include <swarm_msgs/ImageDescriptor.h>
 #include <swarm_msgs/LoopConnection.h>
+#include <swarm_msgs/ImageDescriptor_t.hpp>
 
 using namespace swarm_msgs;
 
 class LoopDetector {
 public:
     LoopDetector();
-    void on_image_recv(const ImageDescriptor & img_des);
+    void on_image_recv(const ImageDescriptor_t & img_des);
     void on_loop_connection(const LoopConnection & loop_conn);
 };
