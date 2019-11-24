8627bdd5529e83380bd42ecb9d84b90a3e0d2065
XU Hao <xuhao3e8@gmail.com>
2019-11-24 16:27:03 +0800

    Add debug image param

--- swarm_loop/src/swarm_loop_node.cpp
+++ swarm_loop/src/swarm_loop_node.cpp
@@ -90,6 +90,7 @@ public:
         loop_cam = new LoopCam(camera_config_path, BRIEF_PATTHER_FILE);
         loop_detector = new LoopDetector(ORB_VOC);
         loop_detector->loop_cam = loop_cam;
+        loop_detector->enable_visualize = debug_image;
 
         camera_sub = nh.subscribe("left_camera", 1000, &SwarmLoopNode::image_callback, this);
         viokeyframe_sub = nh.subscribe("/vins_estimator/viokeyframe", 1000, &SwarmLoopNode::VIOKF_callback, this);
