c784b0f0454f8566be2c688769413c89bc87ad3c
XU Hao <xuhao3e8@gmail.com>
2019-12-12 22:44:36 +0800

    Improving...

--- swarm_loop/include/loop_defines.h
+++ swarm_loop/include/loop_defines.h
@@ -19,8 +19,8 @@
 #define MAX_LOOP_DIS 5.0 //ACCEPT MAX DISTANCE, 2.0 for indoor flying
 
 #define INIT_MODE_MIN_LOOP_NUM 12 //Init mode we accepte this inlier number
-#define INIT_MODE_MIN_LOOP_NUM_L2 8 //Init mode we accepte this inlier number
-#define MAX_LOOP_DIS_L2 3.0 //ACCEPT MAX DISTANCE, 2.0 for indoor flying
+#define INIT_MODE_MIN_LOOP_NUM_LEVEL2 6 //Init mode we accepte this inlier number
+#define MAX_LOOP_DIS_LEVEL2 3.0 //ACCEPT MAX DISTANCE, 2.0 for indoor flying
 
 #define DEG2RAD (0.01745277777777778)
 
@@ -31,13 +31,13 @@
 #define DEEP_DESC_SIZE 1024
 
 #define SEARCH_NEAREST_NUM 5
-#define INNER_PRODUCT_THRES 0.85
+#define INNER_PRODUCT_THRES 0.75
 #define ACCEPT_NONKEYFRAME_WAITSEC 5.0
 
 
 #define INIT_MODE_PRODUCT_THRES 0.5 //INIT mode we can accept this inner product as similar
 
-#define ORB_HAMMING_DISTANCE 80 //Max hamming
+#define ORB_HAMMING_DISTANCE 40 //Max hamming
 #define ORB_UV_DISTANCE 1.5 //UV distance bigger than mid*this will be removed
 
 #define VISUALIZE_SCALE 1 //Scale for visuallize
@@ -45,10 +45,10 @@
 #define CROP_WIDTH_THRES 0.05 //If movement bigger than this, crop some matches down
 
 #define OUTLIER_XY_PRECENT_0 0.05 // This is given up match dx dy 
-#define OUTLIER_XY_PRECENT_20 0.10 // This is given up match dx dy 
-#define OUTLIER_XY_PRECENT_30 0.10 // This is given up match dx dy 
-#define OUTLIER_XY_PRECENT_40 0.10 // This is given up match dx dy 
+#define OUTLIER_XY_PRECENT_20 0.1 // This is given up match dx dy 
+#define OUTLIER_XY_PRECENT_30 0.1 // This is given up match dx dy 
+#define OUTLIER_XY_PRECENT_40 0.1 // This is given up match dx dy 
 
-#define PNP_REPROJECT_ERROR 15.0
+#define PNP_REPROJECT_ERROR 10.0
 #define AVOID_GROUND_PRECENT 0.666 // This is for avoiding detect a lot feature on ground
 // #define DEBUG_SHOW_IMAGE
