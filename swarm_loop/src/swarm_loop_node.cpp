821464726002f1f39d991789cb3ebcbccbd3574c
Xu Hao <xuhao3e8@gmail.com>
2020-10-16 16:22:17 +0800

    Cached

--- swarm_loop/src/swarm_loop_node.cpp
+++ swarm_loop/src/swarm_loop_node.cpp
@@ -158,13 +158,16 @@ public:
     bool enable_pub_remote_img;
     bool enable_sub_remote_img;
     bool send_img;
+    bool send_whole_img_desc;
     std::thread th;
 
     double max_freq = 1.0;
     double recv_msg_duration = 0.5;
 
+    ros::Timer timer;
 public:
     SwarmLoopNode () {}
+    
 private:
     virtual void onInit() {
         //Init Loop Net
@@ -187,9 +190,10 @@ private:
         nh.param<int>("jpg_quality", JPG_QUALITY, 50);
         nh.param<int>("accept_min_3d_pts", ACCEPT_MIN_3D_PTS, 50);
         nh.param<bool>("enable_lk", ENABLE_LK_LOOP_DETECTION, true);
-        nh.param<bool>("enable_pub_remote_img", enable_pub_remote_img, true);
+        nh.param<bool>("enable_pub_remote_img", enable_pub_remote_img, false);
         nh.param<bool>("enable_sub_remote_img", enable_sub_remote_img, false);
         nh.param<bool>("send_img", send_img, false);
+        nh.param<bool>("send_whole_img_desc", send_whole_img_desc, false);
         nh.param<double>("query_thres", INNER_PRODUCT_THRES, 0.6);
         nh.param<double>("init_query_thres", INIT_MODE_PRODUCT_THRES, 0.3);
         nh.param<double>("min_movement_keyframe", MIN_MOVEMENT_KEYFRAME, 0.2);
@@ -205,7 +209,7 @@ private:
 
         nh.param<bool>("debug_image", debug_image, false);
         
-        loop_net = new LoopNet(_lcm_uri, send_img, recv_msg_duration);
+        loop_net = new LoopNet(_lcm_uri, send_img, send_whole_img_desc, recv_msg_duration);
         loop_cam = new LoopCam(camera_config_path, BRIEF_PATTHER_FILE, self_id, send_img, nh);
         loop_cam->show = debug_image; 
 #ifdef USE_DEEPNET
@@ -246,6 +250,10 @@ private:
         if (enable_pub_remote_img) {
             remote_image_desc_pub = nh.advertise<swarm_msgs::ImageDescriptor>("remote_image_desc", 10);
         }
+
+        timer = nh.createTimer(ros::Duration(0.01), [&](auto e) {
+            loop_net->scan_recv_packets();
+        });
 
         th = std::thread([&] {
             while(0 == loop_net->lcm_handle()) {
