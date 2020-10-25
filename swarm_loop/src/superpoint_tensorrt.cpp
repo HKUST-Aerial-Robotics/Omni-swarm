501c864f618318808e286147cc70aac6c8359bb9
XU Hao <xuhao3e8@gmail.com>
2020-10-25 23:06:16 +0800

    Fixed superpoint

--- swarm_loop/src/superpoint_tensorrt.cpp
+++ swarm_loop/src/superpoint_tensorrt.cpp
@@ -125,7 +125,9 @@ void SuperPointTensorRT::inference(const cv::Mat & input, std::vector<cv::Point2
         input.convertTo(_input, CV_32F, 1/255.0);
     }
     doInference(_input);
-    std::cout << "Inference Time " << tic.toc();
+    if (enable_perf) {
+        std::cout << "Inference Time " << tic.toc();
+    }
 
     TicToc tic1;
     auto options = torch::TensorOptions().dtype(torch::kFloat32);
@@ -134,42 +136,53 @@ void SuperPointTensorRT::inference(const cv::Mat & input, std::vector<cv::Point2
     
     auto mProb = at::from_blob(m_OutputTensors[0].hostBuffer, {1, 1, height, width}, options);
     auto mDesc = at::from_blob(m_OutputTensors[1].hostBuffer, {1, 256, height/8, width/8}, options);
-    std::cout << " from_blob " << tic1.toc();
+    cv::Mat Prob = cv::Mat(height, width, CV_32F, m_OutputTensors[0].hostBuffer);
+    if (enable_perf) {
+        std::cout << " from_blob " << tic1.toc();
+    }
 
     cv::Mat descriptors;
     TicToc tic2;
-    getKeyPoints(mProb, thres, keypoints);
-    std::cout << " getKeyPoints " << tic2.toc();
+    getKeyPoints(Prob, thres, keypoints);
+    if (enable_perf) {
+        std::cout << " getKeyPoints " << tic2.toc();
 
-    cv::Mat heat(height, width, CV_32F, 1);
-    memcpy(heat.data, m_OutputTensors[0].hostBuffer, width*height * sizeof(float));
-    heat.convertTo(heat, CV_8U, 10000);
-    cv::resize(heat, heat, cv::Size(), 2, 2);
-    cv::imshow("Heat", heat);
+        cv::Mat heat(height, width, CV_32F, 1);
+        memcpy(heat.data, m_OutputTensors[0].hostBuffer, width*height * sizeof(float));
+        heat.convertTo(heat, CV_8U, 10000);
+        cv::resize(heat, heat, cv::Size(), 2, 2);
+        cv::imshow("Heat", heat);
+    }
 }
 
-void SuperPointTensorRT::getKeyPoints(const torch::Tensor & mProb, float threshold, std::vector<cv::Point2f> &keypoints)
+void SuperPointTensorRT::getKeyPoints(const cv::Mat & prob, float threshold, std::vector<cv::Point2f> &keypoints)
 {
-    auto kpts = (mProb > threshold);
-    kpts = torch::nonzero(kpts);  // [n_keypoints, 2]  (y, x)
-    std::cout << kpts.sizes();
+    TicToc getkps;
+    auto mask = (prob > threshold);
+    std::vector<cv::Point> kps;
+    cv::findNonZero(mask, kps);
     std::vector<cv::Point2f> keypoints_no_nms;
-    for (int i = 0; i < kpts.size(0); i++) {
-        // std::cout << kpts[i][3].item<float>() <<":" << kpts[i][2].item<float>() << std::endl;
-        keypoints_no_nms.push_back(cv::Point2f(kpts[i][3].item<float>(), kpts[i][2].item<float>()));
+    for (int i = 0; i < kps.size(); i++) {
+        keypoints_no_nms.push_back(cv::Point2f(kps[i].x, kps[i].y));
     }
 
     cv::Mat conf(keypoints_no_nms.size(), 1, CV_32F);
     for (size_t i = 0; i < keypoints_no_nms.size(); i++) {
         int x = keypoints_no_nms[i].x;
         int y = keypoints_no_nms[i].y;
-        conf.at<float>(i, 0) = mProb[0][0][y][x].item<float>();
+        conf.at<float>(i, 0) = prob.at<float>(y, x);
     }
 
+    if (enable_perf) {
+        printf("GetKeyPointsNoNMS %f ", getkps.toc());
+    }
     int border = 0;
     int dist_thresh = 4;
+    TicToc ticnms;
     NMS2(keypoints_no_nms, conf, keypoints, border, dist_thresh, width, height);
-    std::cout << "\nkeypoints no nms" << keypoints_no_nms.size() <<" keypoints nms " << keypoints.size() << std::endl;
+    if (enable_perf) {
+        printf("NMS %f keypoints_no_nms %ld keypoints %ld\n", ticnms.toc(), keypoints_no_nms.size(), keypoints.size());
+    }
 }
 
 
@@ -218,7 +231,7 @@ void NMS2(std::vector<cv::Point2f> det, cv::Mat conf, std::vector<cv::Point2f>& 
     inds.setTo(0);
     confidence.setTo(0);
 
-    for (int i = 0; i < pts_raw.size(); i++)
+    for (unsigned int i = 0; i < pts_raw.size(); i++)
     {   
         int uu = (int) pts_raw[i].x;
         int vv = (int) pts_raw[i].y;
@@ -229,12 +242,10 @@ void NMS2(std::vector<cv::Point2f> det, cv::Mat conf, std::vector<cv::Point2f>& 
         confidence.at<float>(vv, uu) = conf.at<float>(i, 0);
     }
 
-    cv::copyMakeBorder(grid, grid, dist_thresh, dist_thresh, dist_thresh, dist_thresh, cv::BORDER_CONSTANT, 0);
-
     for (int i = 0; i < pts_raw.size(); i++)
     {   
-        int uu = (int) pts_raw[i].x + dist_thresh;
-        int vv = (int) pts_raw[i].y + dist_thresh;
+        int uu = (int) pts_raw[i].x;
+        int vv = (int) pts_raw[i].y;
 
         if (grid.at<char>(vv, uu) != 1)
             continue;
@@ -244,7 +255,7 @@ void NMS2(std::vector<cv::Point2f> det, cv::Mat conf, std::vector<cv::Point2f>& 
             {
                 if(j==0 && k==0) continue;
 
-                if ( confidence.at<float>(vv + k - dist_thresh, uu + j - dist_thresh) < confidence.at<float>(vv-dist_thresh, uu - dist_thresh) ) {
+                if ( confidence.at<float>(vv + k, uu + j) < confidence.at<float>(vv, uu) ) {
                     grid.at<char>(vv + k, uu + j) = 0;
                 }
             }
@@ -253,15 +264,15 @@ void NMS2(std::vector<cv::Point2f> det, cv::Mat conf, std::vector<cv::Point2f>& 
 
     size_t valid_cnt = 0;
 
-    for (int v = 0; v < (img_height + dist_thresh); v++){
-        for (int u = 0; u < (img_width + dist_thresh); u++)
+    for (int v = 0; v < (img_height); v++){
+        for (int u = 0; u < (img_width); u++)
         {
-            if (u -dist_thresh>= (img_width - border) || u-dist_thresh < border || v-dist_thresh >= (img_height - border) || v-dist_thresh < border)
+            if (u>= (img_width - border) || u < border || v >= (img_height - border) || v < border)
             continue;
 
             if (grid.at<char>(v,u) == 2)
             {
-                int select_ind = (int) inds.at<unsigned short>(v-dist_thresh, u-dist_thresh);
+                int select_ind = (int) inds.at<unsigned short>(v, u);
                 cv::Point2f p = pts_raw[select_ind];
                 pts.push_back(p);
                 valid_cnt++;
