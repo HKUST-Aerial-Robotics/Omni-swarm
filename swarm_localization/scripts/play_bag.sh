bcb35200ac943b0f147d8f08055c8dc8c604c32e
Xu Hao <xuhao3e8@gmail.com>
2021-03-01 00:45:36 +0800

    Cached.

--- swarm_localization/scripts/play_bag.sh
+++ swarm_localization/scripts/play_bag.sh
@@ -3,4 +3,4 @@ source /opt/ros/melodic/setup.bash
 source /root/swarm_ws/devel/setup.bash
 BAGPATH=/root/bags/$1
 #/opt/ros/melodic/lib/rosbag/play --queue 100 --rate 1.0 --delay 0.2 --duration $2 /root/bags/$1 --rate-control-max-delay 1.0 --clock& 
-rosrun swarm_localization sync_rosplay.py --path $BAGPATH --syst $2
+rosrun sync_bag_player sync_rosplay.py --path $BAGPATH --syst $2
