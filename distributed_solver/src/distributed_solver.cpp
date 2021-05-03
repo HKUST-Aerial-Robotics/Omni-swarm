d0fc977b8586ed3f472c33fd6cdf8cedd84dfc37
Xu Hao <xuhao3e8@gmail.com>
2021-05-03 15:51:13 +0800

    Added tolerance

--- distributed_solver/src/distributed_solver.cpp
+++ distributed_solver/src/distributed_solver.cpp
@@ -536,13 +536,14 @@ namespace DSLAM {
         }
     }
 
-    double CentrializedSolver::solve() {
+    double CentrializedSolver::solve(double tolerance) {
         ceres::Solver::Options options;
         //SPARSE NORMAL DOGLEG 12.5ms
         //SPARSE NORMAL 21
         //DENSE NORM DOGLEG 49.31ms
         options.max_num_iterations = 1000;
         options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
+        options.function_tolerance = tolerance;
         // options.trust_region_strategy_type = ceres::DOGLEG;
         options.num_threads = 1;
         ceres::Solver::Summary summary;
